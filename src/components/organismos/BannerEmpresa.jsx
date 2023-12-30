be6bd8c7034a5d2c80e44fd560f013e0a0fe18ff
Furry <92775533+Manuel-F-G@users.noreply.github.com>
2023-12-30 12:03:15 -0600

    Card
    currency cards and employees

--- src/components/organismos/BannerEmpresa.jsx
+++ src/components/organismos/BannerEmpresa.jsx
@@ -1,20 +1,43 @@
 import styled from "styled-components";
 import { v } from "../../styles/variables"
-export function BannerEmpresa() {
-    return (<Container>
-        <div className="content-wrapper-context">
-            <span className="titulo">
-                {<v.iconoempresa/>}
-                Nombre de la emprezzza
-            </span>
-            <div className="content-text">
-                No se que poner, pero aqui va algo chidote
+import {CardDatosEmpresa} from "../moleculas/CardDatosEmpresa"
+export function BannerEmpresa(){
+    return (
+        <Container>
+            <div className="content-wrapper-context">
+                <span className="titulo">
+                    {<v.iconoempresa />}
+                    Grupo base energeticos
+                </span>
+                <div className="content-text">
+                    Aquí va un texto chidote
+                </div>
+                <ContentCards>
+                    <CardDatosEmpresa titulo="Moneda" valor="Mxn"/>
+                    <CardDatosEmpresa titulo="Usuarios" valor="9999"/>
+                </ContentCards>
             </div>
-            <contentCards>
-
-            </contentCards>
-        </div>
-    </Container>);
-} 
-const Container =styled.div``
-const ContentCards =styled.div``
+            
+        </Container>
+    );
+}
+const Container = styled.div`
+width: 100%;
+height: 100%;
+position: relative;
+display: flex;
+align-items: center;
+justify-content: center;
+border: 0 solid #6b6b6b;
+background-size: contain;
+background-position: center;
+background-repeat: no-repeat, repeat;
+border-radius: 14px;
+overflow: hidden;
+`;
+const ContentCards = styled.div`
+display: flex; 
+gap: 10px;
+padding-top: 15px;
+cursor: pointer;
+`;
