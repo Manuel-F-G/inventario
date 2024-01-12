beae4bcd23533789ed18a5feae5037824ed49427
Furry <92775533+Manuel-F-G@users.noreply.github.com>
2024-01-12 12:32:06 -0600

    Skeleton of the tables
    It already eliminates things and well, gem

--- src/components/organismos/tablas/TablaMarca.jsx
+++ src/components/organismos/tablas/TablaMarca.jsx
@@ -7,8 +7,36 @@ import {
     flexRender,     
 } from "@tanstack/react-table";
 import styled from "styled-components";
-import {ContentAccionesTabla} from "../../../index"
+import {ContentAccionesTabla, useMarcaStore} from "../../../index"
+import Swal from "sweetalert2";
 export function TablaMarca ({data}){
+    const {eliminarMarca} = useMarcaStore()
+    const editar = () => {
+
+    }
+    const eliminar = (p) => {
+        if(p.descripcion==="Generica"){
+            Swal.fire({
+                icon: "error",
+                title: "Chin...",
+                text: "No puedes eliminar este registro ya que es un valor por defecto",
+            });
+            return;
+        }
+        Swal.fire({
+            title: "¿Estas seguro?",
+            text: "No podras revertir esta acción",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Si, !Eliminar!",
+        }).then(async(result) => {
+            if (result.isConfirmed) {
+                    await eliminarMarca({id:p.id})
+            }
+        });    
+    };
     const columns=[{
         accessorKey: "descripcion",
         header: "Descripción",
@@ -17,10 +45,15 @@ export function TablaMarca ({data}){
     {
         accessorKey: "acciones",
         header: "Acciones",
-        cell:(info)=>(<td>
-            <ContentAccionesTabla/>
-        </td>)
-    }
+        cell:(info)=>(
+        <td>
+            <ContentAccionesTabla 
+            funcionEditar={()=>editar(info.row.original)}
+            funcionEliminar={()=>eliminar(info.row.original)}
+            />
+        </td>
+        ),
+    },
 ];
     const table = useReactTable({
         data,
@@ -66,5 +99,7 @@ export function TablaMarca ({data}){
         </table>
     </Container>);
 }
+
+
 const Container = styled.div`
 `
