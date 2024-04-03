1b0babd2ab3a37ad400462b48faba25a1a26d747
Furry <92775533+Manuel-F-G@users.noreply.github.com>
2024-04-03 12:35:28 -0600

    Update TablaKardex.jsx

--- src/components/organismos/tablas/TablaKardex.jsx
+++ src/components/organismos/tablas/TablaKardex.jsx
@@ -73,7 +73,7 @@ export function TablaKardex({
     },
     {
       accessorKey: "acciones",
-      header: "",
+      header: "Acciones",
       enableSorting:false,
       cell: (info) => (
         <td className="ContentCell">
